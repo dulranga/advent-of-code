7b5bb0938e2c4a1e90833732e153c2dfd63676c1
dulranga <dulranga.webdesign@gmail.com>
2024-05-06 10:57:41 +0530

    finished challenge

--- day1/main.go
+++ day1/main.go
@@ -5,10 +5,14 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"regexp"
+	"strconv"
 	"strings"
 )
 
 func Day1() {
+
+	digitRegex := regexp.MustCompile(`\d`)
 
 	path, err := filepath.Abs("day1/input.txt")
 	if err != nil {
@@ -20,11 +24,38 @@ func Day1() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	content := string(rawData)
 
 	values := strings.Split(content, "\n")
 
+	// holds the sum of all calibrated values
+	var sum int64
+
 	for v := range values {
-		fmt.Printf("values[v]: %v\n", values[v])
+		alteredText := values[v]
+
+		// -1 to get all matches
+		digits := digitRegex.FindAllString(alteredText, -1)
+
+		if len(digits) == 0 {
+			continue
+		}
+
+		firstDigit := digits[0]
+		lastDigit := digits[len(digits)-1]
+
+		// string concatenation to get the 2 digit number
+		calibrationString := firstDigit + lastDigit
+
+		calibrationValue, err := strconv.ParseInt(calibrationString, 10, 8)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		sum += calibrationValue
+
 	}
+
+	fmt.Printf("Sum: %v\n", sum)
 }
